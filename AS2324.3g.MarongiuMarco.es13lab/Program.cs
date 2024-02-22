5b88bcde7b0607b5ddc5d248c91a98995a236fa2
marko marongiu <coentro07@gmail.com>
2024-02-22 09:04:45 +0100

    statistiche

--- AS2324.3g.MarongiuMarco.es13lab/Program.cs
+++ AS2324.3g.MarongiuMarco.es13lab/Program.cs
@@ -8,7 +8,10 @@
             int n=Convert.ToInt32(Console.ReadLine());
             int[] pesi=new int[n];
             int[] eta=new int[n];
-            CaricaVettori(ref pesi, ref eta);
+            CaricaVettori(ref pesi, ref eta); //carica dei vettori
+            int min = pesi[0], max=pesi[0];
+            double media=0;
+            Statistiche (ref pesi, ref media, ref min, ref max );
 
         }
         static void CaricaVettori(ref int[] pesi,ref int[] eta)
@@ -19,6 +22,25 @@
                 eta[i] = random.Next(50, 101);
                 pesi[i] = random.Next(18, 100);
             }
+        }
+        static void Statistiche(ref int[] pesi, ref double media,ref int min,ref int max)
+        {
+            int somma = 0;
+            int cont = 0;
+            foreach (int i in pesi)
+            {
+                if (pesi[i] > max)
+                {
+                    max = pesi[i];
+                }
+                if (pesi[i] < min)
+                {
+                    min = pesi[i];
+                }
+                somma += pesi[i];
+                cont++;
+            }
+            media = somma / cont;
         }
     }
 }
