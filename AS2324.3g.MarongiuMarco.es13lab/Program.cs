acd0e123b66dedbd089de1433c3b212a902bc2bf
marko marongiu <coentro07@gmail.com>
2024-02-22 08:54:52 +0100

    vettori caricati

--- AS2324.3g.MarongiuMarco.es13lab/Program.cs
+++ AS2324.3g.MarongiuMarco.es13lab/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("quante persone?");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] pesi=new int[n];
+            int[] eta=new int[n];
+            CaricaVettori(ref pesi, ref eta);
+
+        }
+        static void CaricaVettori(ref int[] pesi,ref int[] eta)
+        {
+            Random random = new Random();
+            foreach(int i in pesi)
+            {
+                eta[i] = random.Next(50, 101);
+                pesi[i] = random.Next(18, 100);
+            }
         }
     }
 }
